06057ad8b46e06c71a392b6e89fb11bc05055906
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2025-11-22 16:25:50 +0100

    [frontend] Add click on file

--- composeApp/src/commonMain/kotlin/eu/jstahl/clarifile/frontend/App.kt
+++ composeApp/src/commonMain/kotlin/eu/jstahl/clarifile/frontend/App.kt
@@ -3,6 +3,7 @@ package eu.jstahl.clarifile.frontend
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
@@ -138,7 +139,10 @@ fun App(storage: Storage) {
                             .background(
                                 color = Color.LightGray,
                                 shape = RoundedCornerShape(12.dp)
-                            ),
+                            )
+                            .clickable {
+                                file.open()
+                            },
                     ) {
                         Column(
                             modifier = Modifier
